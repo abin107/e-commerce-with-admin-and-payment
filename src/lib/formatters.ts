35e759a278cce33748b0faa69d99c514a7654615
Abin Abraham <abin2abraham@yahoo.co.in>
2024-04-07 03:36:13 +0530

    created basic admin dashboard page

---
+++ src/lib/formatters.ts
@@ -0,0 +1,15 @@
+const CURRENT_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 0
+});
+
+export function formatCurrency(amount: number) {
+  return CURRENT_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
+
+export function formatNumber(amount: number) {
+  return NUMBER_FORMATTER.format(amount);
+}
